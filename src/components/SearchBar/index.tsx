9d45887a25fbe06bd32ee784a9a9a27d57d55f66
Mihai <mihai807@mail.ru>
2024-05-18 12:53:17 +0300

    fix

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { ChangeEvent, ChangeEventHandler, useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { AppDispatch, RootState } from '../../redux/store';
 import User, { get } from '../../redux/User';
@@ -6,35 +6,17 @@ import UserCard from '../UserCard';
 import './style.scss';
 
 interface SearchProps {
-  onTextChange: (text: string) => void;
+    onTextChange: (e: ChangeEvent<HTMLInputElement>) => void;
+    value: string;
 }
 
-const SearchBar: React.FC<SearchProps>  = ({ onTextChange }) => {
-
-  const [searchText, setSearchText] = useState("Search");
-
-  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setSearchText(event.target.value);
-    onTextChange(event.target.value);
-  };
-
-  const onClick = (event: React.MouseEvent<HTMLElement>) => {
-    setSearchText("");
-    onTextChange("");
-  };
-
-  return (
-    <div className="search-bar">
-      <input type="text" id="search" 
-        name="search" 
-        className='search-input' 
-        value={searchText}
-        onClick={onClick}
-        onChange={onChange}
-      />
-      <div className='refresh-button'>Refresh Users</div>
-    </div>
-  );
+const SearchBar: React.FC<SearchProps> = ({ onTextChange, value }) => {
+    return (
+        <div className="search-bar">
+            <input type="text" id="search" name="search" className="search-input" value={value} onChange={onTextChange} />
+            <div className="refresh-button">Refresh Users</div>
+        </div>
+    );
 };
 
 export default SearchBar;
--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -3,28 +3,28 @@ import SearchBar from '../../components/SearchBar';
 import StatPanel from '../../components/StatPanel';
 import UserCardsPanel from '../../components/UserCardsPanel';
 import './style.scss';
-
-
+//@ts-ignore
+import deleteIcon from './delete.svg';
 
 const Main = () => {
+    const [searchText, setSearchText] = useState('');
 
-  const [searchText, setSearchText] = useState('');
-
-  const handleSearchTextChange = (newText: string) => {
-    setSearchText(newText);
-  };
+    const handleSearchTextChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setSearchText(e.target.value);
+    };
 
     return (
-      <>
-      <div className='main'>
-        <SearchBar onTextChange={handleSearchTextChange}></SearchBar>
-      <div className='main-users'>
-        <UserCardsPanel searchText={searchText}></UserCardsPanel>
-        <StatPanel></StatPanel>
-      </div>
-      </div>
-      </>
+        <>
+            <div className="main">
+                {/* <img height={100} src={deleteIcon} alt="" /> */}
+                <SearchBar onTextChange={handleSearchTextChange} value={searchText}></SearchBar>
+                <div className="main-users">
+                    <UserCardsPanel searchText={searchText}></UserCardsPanel>
+                    <StatPanel />
+                </div>
+            </div>
+        </>
     );
-  };
+};
   
   export default Main;
